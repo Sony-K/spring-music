67d1e37d4012e8b4e6e789a536da0f34c470c449
sony-k <kasala.sony24@gmail.com>
2018-09-14 16:00:42 +0530

    Implemented Oauth2 security

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -4,7 +4,9 @@ import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
